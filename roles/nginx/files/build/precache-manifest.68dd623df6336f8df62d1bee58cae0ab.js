905551539243b16f60590813eb9efd211b41ba31
sivakiran9 <sivakiran1995@gmail.com>
2020-11-01 19:53:02 +0530

    nginx role is added

---
+++ roles/nginx/files/build/precache-manifest.68dd623df6336f8df62d1bee58cae0ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f2af3c9593e329f8defe",
+    "url": "/static/js/main.f2af3c95.chunk.js"
+  },
+  {
+    "revision": "5909bfa0b60bd5fdca7b",
+    "url": "/static/js/1.5909bfa0.chunk.js"
+  },
+  {
+    "revision": "f2af3c9593e329f8defe",
+    "url": "/static/css/main.acd36fb2.chunk.css"
+  },
+  {
+    "revision": "5909bfa0b60bd5fdca7b",
+    "url": "/static/css/1.96d2d25f.chunk.css"
+  },
+  {
+    "revision": "a42cef46c83ac4161b30fdb6f8aea435",
+    "url": "/index.html"
+  }
+];
